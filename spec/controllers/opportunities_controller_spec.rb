556e930e01b13aa67d0c25010e6c5d5db0d55898
Lauren Fazah <laurenfazah@gmail.com>
2024-11-06 14:27:55 -0700

    Assert original opportunity data response for Demo Carrier

--- spec/controllers/opportunities_controller_spec.rb
+++ spec/controllers/opportunities_controller_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 RSpec.describe OpportunitiesController do
   describe "#show" do
-    let(:carrier_id) { "1" }
-    let(:opportunity_id) { "1" }
-
     let(:params) do
       { id: opportunity_id, carrier_id: carrier_id }
     end
 
     context "when carrier is Acme" do
+      let(:carrier_id) { "1" }
+      let(:opportunity_id) { "1" }
+
       let(:expected) do
         {
           id: "1",
@@ -28,6 +28,53 @@ RSpec.describe OpportunitiesController do
               benefits: {
                 commissions: "0.04",
                 xray: "20%"
+              }
+            }
+          ]
+        }
+      end
+
+      before do
+        get :show, params: params
+      end
+
+      it "returns 200" do
+        expect(response).to have_http_status(:ok)
+      end
+
+      it "includes opportunity data in the response" do
+        parsed_response = JSON.parse(
+          response.body,
+          symbolize_names: true
+        )
+
+        expect(parsed_response[:opportunity_data])
+          .to eq expected
+      end
+    end
+
+    context "when carrier is Demo Carrier" do
+      let(:carrier_id) { "2" }
+      let(:opportunity_id) { "2" }
+
+      let(:expected) do
+        {
+          id: "2",
+          coverages: [
+            {
+              id: "702",
+              product_type: "Vision",
+              benefits: {
+                commissions: "$1 PEPM",
+                frames: "Not Included"
+              }
+            },
+            {
+              id: "703",
+              product_type: "Dental",
+              benefits: {
+                commissions: "Flat $1000",
+                x_ray_coinsurance: "30%"
               }
             }
           ]
